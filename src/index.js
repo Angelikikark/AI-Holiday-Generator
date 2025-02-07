6e11f5064d7c9d92e347dc886f04b3cde6adf153
Angelikikark <167430380+Angelikikark@users.noreply.github.com>
2025-02-07 21:56:22 +0000

    Added Js and amend html and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("Hello Word");
+function generateHoliday(event) {
+  event.preventDefault();
+
+  const holidayTypeInput = document.querySelector("input[type='text']").value;
+
+  const holidayMessage = `Based on your love for ${holidayTypeInput}, here’s a holiday idea: Hiking in the Swiss Alps, exploring small villages, and enjoying local Swiss cuisine.`;
+
+  // Clear previous text
+  document.getElementById("holiday").innerHTML = "";
+
+  new Typewriter("#holiday", {
+    strings: [based],
+    autoStart: true,
+    delay: 75,
+    cursor: "",
+  });
+}
+
+let holidayFormElement = document.querySelector("#holiday-generator-form");
+holidayFormElement.addEventListener("submit", generateHoliday);
